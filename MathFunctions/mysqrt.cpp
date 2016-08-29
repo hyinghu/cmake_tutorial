5bdd42ef688dea50f940152d9e9777bb8b5ca4e1
Henry Zhang <zhanghui629@gmail.com>
2016-08-29 14:59:15 +0800

    Adding a Generated File and Generator.

--- MathFunctions/mysqrt.cpp
+++ MathFunctions/mysqrt.cpp
@@ -1,6 +1,10 @@
 #include "MathFunctions.h"
 #include "TutorialConfig.h"
 #include <stdio.h>
+
+// include the generated table
+#include "Table.h"
+
 #include <math.h>
 
 // a hack square root calculation using simple operations
@@ -11,26 +15,24 @@ double mysqrt(double x)
 	}
 
 	double result;
-
-	// if we have both log and exp then use them
-#if defined(HAVE_LOG) && defined(HAVE_EXP)
-	result = exp(log(x) * 0.5);
-	fprintf(stdout, "Computing sqrt of %g, to be %g useing log\n", x, result);
-#else
 	double delta;
-	result = x;
 
-	// do ten interations
+	// use the table to help find an initial value
+	result = x;
+	if (x >= 1 && x < 10) {
+	result = sqrtTable[static_cast<int>(x)];
+	}
+
+	// do ten iterations
 	for (int i = 0; i < 10; ++i) {
 		if (result <= 0) {
 			result = 0.1;
 		}
 
 		delta = x - (result * result);
-		result = result + 0.5 * delta /result;
+		result = result + 0.5 * delta / result;
 		fprintf(stdout, "Computing sqrt of %g to be %g\n", x, result);
 	}
-#endif
 
 	return result;
 }
